443d8023aaeec3afddeb6e248ba5dae87110ebf0
gracebir <grace.devolop@gmail.com>
2024-05-01 13:35:29 +0200

    calling navbar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 /** @format */
 
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-    return <div></div>;
+    return (
+        <div className='relative h-screen font-inter'>
+            <Navbar />
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
